1238b5286720e49ca6ef471ac88623a4e4c5fb14
MoreThanTom <tomsputz@outlook.com>
2018-09-20 10:34:54 +0100

    Improved index.js No unneeded modules, and better async

--- index.js
+++ index.js
@@ -6,54 +6,53 @@ if (Number(process.version.slice(1).split(".")[0]) < 8) throw new Error("Node 8.
 // Load up the discord.js library
 const Discord = require("discord.js");
 // We also load the rest of the things we need in this file:
-const { promisify } = require("util");
-const readdir = promisify(require("fs").readdir);
+const { readdir } = require("fs");
 const Enmap = require("enmap");
 
-// This is your client. Some people call it `bot`, some people call it `self`,
-// some might call it `cootchie`. Either way, when you see `client.something`,
-// or `bot.something`, this is what we're refering to. Your client.
-const client = new Discord.Client();
+// Async function to start bot with. Allows bot to be run below top-level node process without blocking
+async function init() {
+  // This is your client. Some people call it `bot`, some people call it `self`,
+  // some might call it `cootchie`. Either way, when you see `client.something`,
+  // or `bot.something`, this is what we're refering to. Your client.
+  const client = new Discord.Client();
 
-// Here we load the config file that contains our token and our prefix values.
-client.config = require("./config.js");
-// client.config.token contains the bot's token
-// client.config.prefix contains the message prefix
+  // Here we load the config file that contains our token and our prefix values.
+  client.config = require("./config.js");
+  // client.config.token contains the bot's token
+  // client.config.prefix contains the message prefix
 
-// Require our logger
-client.logger = require("./modules/Logger");
+  // Require our logger
+  client.logger = require("./modules/Logger");
 
-// Let's start by getting some useful functions that we'll use throughout
-// the bot, like logs and elevation features.
-require("./modules/functions.js")(client);
+  // Let's start by getting some useful functions that we'll use throughout
+  // the bot, like logs and elevation features.
+  require("./modules/functions.js")(client);
 
-// Aliases and commands are put in collections where they can be read from,
-// catalogued, listed, etc.
-client.commands = new Enmap();
-client.aliases = new Enmap();
+  // Aliases and commands are put in collections where they can be read from,
+  // catalogued, listed, etc.
+  client.commands = new Enmap();
+  client.aliases = new Enmap();
 
-// Now we integrate the use of Evie's awesome Enhanced Map module, which
-// essentially saves a collection to disk. This is great for per-server configs,
-// and makes things extremely easy for this purpose.
-client.settings = new Enmap({name: "settings"});
-
-// We're doing real fancy node 8 async/await stuff here, and to do that
-// we need to wrap stuff in an anonymous function. It's annoying but it works.
-
-const init = async () => {
+  // Now we integrate the use of Evie's awesome Enhanced Map module, which
+  // essentially saves a collection to disk. This is great for per-server configs,
+  // and makes things extremely easy for this purpose.
+  client.settings = new Enmap({name: "settings"});
 
   // Here we load **commands** into memory, as a collection, so they're accessible
   // here and everywhere else.
-  const cmdFiles = await readdir("./commands/");
+  // I've looked into making loading commands asynchronous, however it doesn't really
+  // provide any benefit until ~20 commands are being loaded, so I am going to keep
+  // the code synchronous for simplicity
+  const cmdFiles = await new Promise(res => readdir("./commands/", (e, files) => res(files)));
   client.logger(`Loading a total of ${cmdFiles.length} commands.`);
   cmdFiles.forEach(f => {
     if (!f.endsWith(".js")) return;
     const response = client.loadCommand(f);
     if (response) console.log(response);
   });
-
+  
   // Then we load events, which will include our message and ready event.
-  const evtFiles = await readdir("./events/");
+  const evtFiles = await new Promise(res => readdir("./events/", (e, files) => res(files)));
   client.logger(`Loading a total of ${evtFiles.length} events.`);
   evtFiles.forEach(file => {
     const eventName = file.split(".")[0];
@@ -75,7 +74,7 @@ const init = async () => {
   // Here we login the client.
   client.login(client.config.token);
 
-// End top-level async/await function.
-};
-
+  // End top-level async/await function.
+}
+exports.init = init;
 init();
